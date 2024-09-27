4eec709ba3ff7c4681861bade9058d64ee2a1d43
guiiBecker <guilhermegbecker@gmail.com>
2024-09-27 14:00:23 -0300

    feat. home content

---
+++ src/components/text/index.tsx
@@ -0,0 +1,13 @@
+import { MainContainer } from "../header/style";
+import { Introduction, Title,  } from "./style";
+
+
+
+export default function Text(){
+    return (
+        <MainContainer>
+            <Introduction></Introduction>
+            <Title>Backend Developer</Title>
+        </MainContainer>
+    );
+}
---
+++ src/components/text/style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const Title = styled.h1 `
+ font-size: 30px;
+`;
+
+
+export const About = styled.p `
+
+`;
+
+export const Introduction = styled.p`
+
+
+`;
