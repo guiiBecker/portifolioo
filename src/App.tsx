7fab4934bdf9bda5a44a4577a76ffad30e80d1e1
guiiBecker <guilhermegbecker@gmail.com>
2024-09-27 19:16:57 -0300

    delete. delete components

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import MyButton from './components/button';
 import Header from './components/header';
+import Text from './components/text';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Text></Text>
         <MyButton />
   
     </div>
